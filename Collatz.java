b3a4c980e1b92d8b87c9df5fd8c9a29815f7075c
thg1lb <gameboysam@outlook.com>
2023-08-31 10:31:56 -0400

    odd or even loop

--- Collatz.java
+++ Collatz.java
@@ -4,10 +4,24 @@ import java.util.Random;
 
 public class Collatz
 {
-    int ogNumber = 1;
-    int num = ogNumber;
+    //defining variables
+    public static int ogNumber = 1;
+    public static int num = ogNumber;
 
     public static void main(String[] args) {
-        
+
+        //checks if number is even
+        while(num != 1){
+            if(num % 2 == 0){
+                num = num/2;
+                System.out.println(num);
+            }
+
+            //or odd
+            else {
+                num = num*3 + 1;
+                System.out.println(num);
+            }
+        }
     }
 }
