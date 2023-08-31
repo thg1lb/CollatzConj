121692d90b792f4912d219a511cdb0f38741f75d
thg1lb <gameboysam@outlook.com>
2023-08-31 09:30:11 -0400

    creation + first commit

---
+++ Collatz.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+import java.util.Random;
+
+
+public class Collatz
+{
+    int ogNumber = 1;
+    int num = ogNumber;
+
+    public static void main(String[] args) {
+        
+    }
+}
