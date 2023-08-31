b3249c70deb3c0550f5906e8528920da393d80a1
thg1lb <gameboysam@outlook.com>
2023-08-31 10:48:16 -0400

    hold on issues

--- Collatz.java
+++ Collatz.java
@@ -1,30 +1,10 @@
 import java.util.Scanner;
 
-
 public class Collatz
 {
-
     public static void main(String[] args) 
     {
-        //opening scanner
-        Scanner in = new Scanner(System.in);
-
-        //user input
-        System.out.print("Please enter a whole number >> ");
-        int num = in.nextInt();
-
-        //checks if number is even
-        while(num != 1){
-            if(num % 2 == 0){
-                num = num/2;
-                System.out.println(num);
-            }
-
-            //or odd
-            else {
-                num = num*3 + 1;
-                System.out.println(num);
-            }
-        }
+        System.out.println("Hi");
+       
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,17 @@ Inspired by the Collatz conjecture, this project aims to find the alleged "infin
 As it is in its beginning phase, we are still prototyping.
 
 Currently two people are working on this, one (me) in Java, and the other in C#.
+
+ while(num != 1)
+        {
+            if(num % 2 == 0){
+                num = num/2;
+                System.out.println(num);
+            }
+
+            //or odd
+            else {
+                num = num*3 + 1;
+                System.out.println(num);
+            }
+        }
